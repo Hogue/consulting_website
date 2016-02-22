97fb390391cb6e136b5cae516dac46c2f035b4f6
Hogue <ajhogue6@gmail.com>
2016-02-22 13:19:36 -0500

    adds updated homepage layout

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+resizeDiv();
+});
+
+window.onresize = function(event) {
+resizeDiv();
+}
+
+function resizeDiv() {
+vpw = $(window).width();
+vph = $(window).height();
+$('#landing-wrapper').css({'height': vph + 'px'});
+}
+
+
